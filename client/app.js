a23fe197c2d07dfe4bae817b7c7655df08e205ff
Madhur Dixit <madhurdixit37@gmail.com>
2024-06-22 14:22:56 -0400

    Website done

---
+++ client/app.js
@@ -0,0 +1,71 @@
+function getBathValue() {
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for(var i in uiBathrooms) {
+      if(uiBathrooms[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  
+  function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for(var i in uiBHK) {
+      if(uiBHK[i].checked) {
+          return parseInt(i)+1;
+      }
+    }
+    return -1; // Invalid Value
+  }
+  
+  function onClickedEstimatePrice() {
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+    var area_type = document.getElementById("uiarea_types");
+  
+    var url = "http://127.0.0.1:5000/predict_home_price"; 
+    // var url = "/api/predict_home_price"; 
+  
+    $.post(url, {
+        total_sqft: parseFloat(sqft.value),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location.value,
+        area_type: area_type.value
+    },function(data, status) {
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+  }
+  
+  function onPageLoad() {
+    console.log( "document loaded" );
+    var url = "http://127.0.0.1:5000/get_location_names"; 
+    // var url = "/api/get_location_names"; 
+    $.get(url,function(data, status) {
+        console.log("got response for get_location_names request");
+        if(data) {
+            var area_type = data.area_type;
+            var uiarea_types = document.getElementById("uiarea_types");
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiarea_types').empty();
+            for(var i in area_type) {
+                var opt = new Option(area_type[i]);
+                $('#uiarea_types').append(opt);
+            }
+            $('#uiLocations').empty();
+            for(var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+  }
+  
+  window.onload = onPageLoad;
